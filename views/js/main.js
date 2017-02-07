b4852e166e78632047b68f1d636391fdf62ac722
rutuja13 <thatterutuja2195@gmail.com>
2017-02-07 18:22:29 +0530

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -437,7 +437,7 @@ var resizePizzas = function(size) {
           console.log("bug in sizeSwitcher");
       }
           
-      var randomPizzas = document.querySelectorAll(".randomPizzaContainer");
+      var randomPizzas = document.getElementsByClassName("randomPizzaContainer");
       for (var i = 0; i < randomPizzas.length; i++) {
       randomPizzas[i].style.width = newWidth + "%";
       }
